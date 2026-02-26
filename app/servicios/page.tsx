391dc8332aecfc315663db0a701dcf009051722c
Anthony Gotay <indusstriess@gmail.com>
2026-02-26 11:44:25 +0100

    Revision en fuentes, piloto de servicios integrado 2

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,9 +1,12 @@
+import FadeInSection from "@/components/tabula-rasa/FadeInSection"
 import ServiciosComponent from "@/components/tabula-rasa/Servicios"
 
 const servicios = () => {
   return (
     <section>
+      <FadeInSection>
       <ServiciosComponent />
+      </FadeInSection>
     </section>
   )
 }
