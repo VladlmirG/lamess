8ec2ffb714ca160ea4f95a187d1e501b7a69a38d
Anthony Gotay <indusstriess@gmail.com>
2026-02-28 21:31:45 +0100

    Estancia nosotros piloto 2

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,16 +1,20 @@
-import HistoriaSection from "@/components/tabula-rasa/HistoriaSection"
-import NosotrosMisc1 from "@/components/tabula-rasa/NosotrosMisc1"
-import TeamSection from "@/components/tabula-rasa/TeamSection"
+"use client";
 
+import { useEffect } from "react";
+import HistoriaSection from "@/components/tabula-rasa/HistoriaSection";
+import NosotrosMisc1 from "@/components/tabula-rasa/NosotrosMisc1";
+import TeamSection from "@/components/tabula-rasa/TeamSection";
 
-const nosotros = () => {
+export default function Nosotros() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
-      <TeamSection/>
-      <HistoriaSection/>
-      <NosotrosMisc1/>
+      <TeamSection />
+      <HistoriaSection />
+      <NosotrosMisc1 />
     </div>
-  )
+  );
 }
-
-export default nosotros
